ebddc08d7d55e9e237f4af47fc4c6fb286281591
Ana Resende <anaresende93@gmail.com>
2021-08-23 10:53:21 +0100

    feat: add basic html and css

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+// 1 - Getting the canvas and create its context and initialize other variables
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+
+}
+
