62f35ce7734252006c5a35b42f89e5b23119db1e
Ana Resende <anaresende93@gmail.com>
2021-08-26 16:34:45 +0100

    fix: button size and position for the gameboy

--- js/events.js
+++ js/events.js
@@ -7,36 +7,7 @@ function readInstructions() {
 }
 
 
-
-
-// // START GAME
-// function startGame() {
-//     runningGame();
-//     audioClick.play();
-//     audioGameplay.play();
-
-//     // changing from the splash page to the gameplay
-//     document.getElementById("game-instructions").classList.remove("show");
-//     document.getElementById("game-over").classList.remove("show");
-//     document.getElementById("game-intro").classList.remove("show");
-//     document.getElementById("game-play").classList.add("show");
-    
-
-//     // Setting the interval for the opponents appear only when click start button
-
-//     opponentsId = setInterval(function () {
-//         let opponentY = getRandomNumber(20, 187)
-//         //let opponentY = getRandomNumber(10, 150) // for tests only
-//         let opponent = new Opponent(ctx, canvas.width, opponentY, 2.5);
-//         opponentArray.push(opponent);
-
-//     },1000)
-    
-//     gameStarted = 1;
-
-// };
-
-// RESTART GAME
+// START - RESTART GAME
 function startGame() {
     audioClick.play();
     audioGameplay.play();
