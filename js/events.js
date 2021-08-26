2ca66470f9aab3cef8f6bc62dc8e1e516fe1cdd6
Ana Resende <anaresende93@gmail.com>
2021-08-26 10:44:03 +0100

    feat : add buttons to the gameboy

--- js/events.js
+++ js/events.js
@@ -53,55 +53,78 @@ function restartGame() {
     runningGame();
 }
 
+// put the movement of the player im a function so that it can be used also with the buttons
+function goUp() {
+    if (player.y > 20){
+        player.y -= 15; 
+        if (ball) {
+            ball.y = player.y
+        }
+    } 
+}
+
+function goDown() {
+    if (player.y < canvas.height - 63){
+        player.y += 15; 
+        if (ball){
+            ball.y = player.y;
+        }
+     } 
+}
+
+function goRight() {
+    if (player.x < 75) {
+        player.x += 15;
+        if (ball) {
+            ball.x = player.x + 15;
+        }
+    }
+}
+
+function goLeft() {
+    if (player.x > 25){
+        player.x -= 15;  
+        if (ball){
+            ball.x = player.x + 15;
+        }
+    } 
+}
+
+function shotBall() {
+    if (ball){ 
+        ball.speedX = 2;
+        ball.speedY = 1.5;
+
+        setTimeout(() => {
+             ball.speedY = -1.5;
+        }, 600);
+    }
+}
 
 //  Paint the player - prepare the keypress 
 document.addEventListener('keydown', (event) => {
 
     if (event.code === 'ArrowUp') {
         event.preventDefault();
-        if (player.y > 20){
-            player.y -= 15; 
-            if (ball) {
-                ball.y = player.y
-            }
-        } 
+        goUp();
    
     } else if (event.code === 'ArrowDown') {
         event.preventDefault();
-        if (player.y < canvas.height - 63){
-           player.y += 15; 
-           if (ball){
-               ball.y = player.y;
-           }
-        } 
+        doDown();
+        
     
     } else if (event.code === 'ArrowRight') {
         event.preventDefault();
-        if (player.x < 75) {
-            player.x += 15;
-            if (ball) {
-                ball.x = player.x + 15;
-            }
-        }
+        goRight();
     
     } else if (event.code === 'ArrowLeft') {
         event.preventDefault();
-        if (player.x > 25){
-            player.x -= 15;  
-            if (ball){
-                ball.x = player.x + 15;
-            }
-        } 
+        goLeft();
     
     }else if (event.code === 'Space') {
         event.preventDefault();
-        if (ball){ 
-            ball.speedX = 2;
-            ball.speedY = 1.5;
-
-            setTimeout(() => {
-                 ball.speedY = -1.5;
-            }, 600);
-        }
+        shotBall();
     } 
 })
+
+
